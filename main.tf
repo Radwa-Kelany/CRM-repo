a041f8c78dce210f8ba2156bbc9df002b404037a
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-07 13:23:40 +0300

    use resource name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_key_pair" "t_key" {
 resource "aws_instance" "web" {
   ami           = "ami-0c1ac8a41498c1a9c"
   instance_type = "t3.micro"
-  key_name = data.aws_key_pair.t_key.key_name
+  key_name = aws_key_pair.t_key.key_name
   tags = {
     Name = "t-ec2-02"
   }
