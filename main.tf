b553428491a519482df599c2eda0aadec8286e30
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-07 13:07:41 +0300

    create ec2 with key pair

--- main.tf
+++ main.tf
@@ -9,4 +9,13 @@ resource "aws_vpc" "example" {
 resource "aws_key_pair" "t-key" {
   key_name   = "t-key"
   public_key = file ("~/.ssh/sshkey.pub")
-   }
+}
+
+   resource "aws_instance" "web" {
+  ami           = "ami-0c1ac8a41498c1a9c"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.t-key
+  tags = {
+    Name = "HelloWorld"
+  }
+}
