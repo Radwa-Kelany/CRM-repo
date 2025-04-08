a6e30ff5a447fc707e2a7e5a277804678d22c353
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-08 09:42:19 +0300

    test 012

--- ec.tf
+++ ec.tf
@@ -1,14 +1,14 @@
 
-resource "aws_key_pair" "t_key" {
-  key_name   = "t-key"
-  public_key = file ("~/.ssh/sshkey.pub")
-}
+# resource "aws_key_pair" "t_key" {
+#   key_name   = "t-key"
+#   public_key = file ("~/.ssh/sshkey.pub")
+# }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0c1ac8a41498c1a9c"
-  instance_type = "t3.micro"
-  key_name = aws_key_pair.t_key.key_name
-  tags = {
-    Name = "t-ec2-02"
-  }
-}
+# resource "aws_instance" "web" {
+#   ami           = "ami-0c1ac8a41498c1a9c"
+#   instance_type = "t3.micro"
+#   key_name = aws_key_pair.t_key.key_name
+#   tags = {
+#     Name = "t-ec2-02"
+#   }
+# }
--- vpc.tf
+++ vpc.tf
@@ -31,17 +31,17 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-# resource "aws_route_table" "CRM_public_rt"{
-#   vpc_id = aws_vpc.CRM_vpc.id
+resource "aws_route_table" "CRM_public_rt"{
+  vpc_id = aws_vpc.CRM_vpc.id
 
-#   route {
-#     cidr_block = "10.0.0.0/16"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
-#   tags = {
-#     Name = "CRM_public_rt"
-#   }
-# }
+  # route {
+  #   cidr_block = "10.0.0.0/16"
+  #   gateway_id = aws_internet_gateway.gw.id
+  # }
+  tags = {
+    Name = "CRM_public_rt"
+  }
+}
 
 resource "aws_route_table" "CRM_private_rt"{
   vpc_id = aws_vpc.CRM_vpc.id
