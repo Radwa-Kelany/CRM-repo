6390cbf54b2abc0f7cb88a9048947c301c93fb7c
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-08 08:54:14 +0300

    vpc terraform file

---
+++ vpc.tf
@@ -0,0 +1,63 @@
+resource "aws_vpc" "CRM_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "CRM_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.CRM_vpc.id
+
+  tags = {
+    Name = "CRM_gw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.CRM_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "CRM_public_subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.CRM_vpc.id
+  cidr_block = "10.0.10.0/24"
+
+  tags = {
+    Name = "CRM_private_subnet"
+  }
+}
+
+resource "aws_route_table" "CRM_public_rt"{
+  vpc_id = aws_vpc.CRM_vpc.id
+
+  tags = {
+    Name = "CRM_public_rt"
+  }
+}
+
+resource "aws_route_table" "CRM_private_rt"{
+  vpc_id = aws_vpc.CRM_vpc.id
+
+  tags = {
+    Name = "CRM_private_rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.CRM_private_subnet.id
+  route_table_id = aws_route_table.CRM_private_rt
+}
+
+resource "aws_route_table_association" "public_a_subnet" {
+  subnet_id      = aws_subnet.CRM_public_subnet.id
+  route_table_id = aws_route_table.CRM_public_rt
+}
+
+resource "aws_route_table_association" "public_a_gw" {
+  gateway_id     = aws_internet_gateway.CRM_gw.id
+  route_table_id = aws_route_table.CRM_public_rt.id
+}
