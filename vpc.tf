a12238b846ff7c9d24aa7e3e60b8c10858039084
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-08 09:19:55 +0300

    add gw assosiation

--- vpc.tf
+++ vpc.tf
@@ -34,6 +34,10 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_route_table" "CRM_public_rt"{
   vpc_id = aws_vpc.CRM_vpc.id
 
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
   tags = {
     Name = "CRM_public_rt"
   }
