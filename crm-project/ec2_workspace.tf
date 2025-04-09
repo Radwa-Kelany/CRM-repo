c9c56f7860f0972a237743d88de385371c37e61c
Radwa Kelany <88267460+Radwa2019@users.noreply.github.com>
2025-04-09 14:47:36 +0300

    add workspace

---
+++ crm-project/ec2_workspace.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "web_east" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = lookup(var.type, terraform.workspace)
+  key_name      = aws_key_pair.t_key_east.key_name
+  tags = {
+    Name = lookup(var.tag, terraform.workspace)
+  }
+}
+
+
+variable "type" {
+  type = map(any)
+  default = {
+    default = "t2.micro"
+    prod    = "t2.micro"
+    dev     = "t2.medium"
+  }
+}
+variable "tag" {
+  type = map(any)
+  default = {
+    default = "default"
+    prod    = "prod"
+    dev     = "dev"
+  }
+}
--- crm-project/main.tf
+++ crm-project/main.tf
@@ -2,12 +2,10 @@ module "sg_module" {
   source = "../modules"
 }
 
-resource "aws_instance" "web" {
-  ami="ami-00a929b66ed6e0de6"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [module.sg_module.sg_id]
-}
+# resource "aws_instance" "web" {
+#   ami="ami-00a929b66ed6e0de6"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [module.sg_module.sg_id]
+# }
 
-output "ec2_id" {
-  value = module.sg_module.lb_sg
-}
+
